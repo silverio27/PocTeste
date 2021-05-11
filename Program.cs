b861fe668e43a3313228d88410b6174decb315a6
lucas silverio <lucas.silverio@clouddata.com.vc>
2021-05-11 20:08:47 -0300

    motos adcionadas

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace PocTeste
         {
             Console.WriteLine("Hello World!");
             Console.Write("Teste");
+            Console.Write("MT-03");
+            Console.Write("CB500F");
+            Console.Write("Z400");
         }
     }
 }
